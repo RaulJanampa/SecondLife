9d7eff11134f857422c49388de9c0a9e29d062da
RaulJanampa <raul.janampa@utec.edu.pe>
2025-05-13 16:41:13 -0500

    feat: archivos spring

---
+++ src/main/java/com/example/HackaRec/HackaRecApplication.java
@@ -0,0 +1,13 @@
+package com.example.HackaRec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaRecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackaRecApplication.class, args);
+	}
+
+}
